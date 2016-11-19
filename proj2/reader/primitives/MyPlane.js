3ce778c95c5833faebf86ab6dcbed8dc58286d88
inessousacaldas <ines.sousacaldas@gmail.com>
2016-11-19 14:45:33 +0000

    Plane to +Z direction. Terrain complete

--- proj2/reader/primitives/MyPlane.js
+++ proj2/reader/primitives/MyPlane.js
@@ -12,53 +12,41 @@ function MyPlane(scene, dimX, dimY, partsX, partsY){
 	this.partsY = partsY;
 	this.getSurfacePoint = null;
 	
+	
+	// Direction to +Z
 	var controlPoints = [];
-
-
-    var temp = [
-        [this.dimX/2, -this.dimY/2, 0, 1],
-        [this.dimX/2, this.dimY/2, 0, 1]
-    ];
-
-    controlPoints.push(temp);
-
-
+	
   	temp =  [
         [-this.dimX/2, -this.dimY/2, 0, 1],
+		[-this.dimX/2, 0, 0, 1],
         [-this.dimX/2, this.dimY/2, 0,  1]
     ];
 
+   
     controlPoints.push(temp);
 	
-	//To center in origin
-	/*var temp =  [
-					[-this.dimX/2, -this.dimY/2, 0, 1], 
-					[-this.dimX/2, 0, 0, 1],
-					[-this.dimX/2, this.dimY/2, 0, 1]
-				];
-	
-	controlPoints.push(temp);
-	
 	temp = [
-					[0, -this.dimY/2, 0, 1], 
-					[0, 0, 0, 1],
-					[0, this.dimY/2, 0, 1]
-				];
+		[0, -this.dimY/2, 0, 1], 
+		[0, 0, 0, 1],
+		[0, this.dimY/2, 0, 1]
+	];
 				
 	controlPoints.push(temp);
-				
-	temp = [
-				[this.dimX/2, -this.dimY/2, 0, 1],
-				[this.dimX/2, 0, 0, 1],
-				[this.dimX/2, this.dimY/2, 0, 1]
-			];
 	
-	controlPoints.push(temp);
-	
-	*/
 	
 
-	var nurbsSurface = new CGFnurbsSurface(1, 1, [0,0,1,1], [0,0,1,1]);
+	 var temp = [
+        [this.dimX/2, -this.dimY/2, 0, 1],
+		[this.dimX/2, 0, 0, 1],
+        [this.dimX/2, this.dimY/2, 0, 1]
+    ];
+
+
+    controlPoints.push(temp);
+	
+	var knots = this.getKnotsVector(2)
+
+	var nurbsSurface = new CGFnurbsSurface(2, 2, knots, knots, controlPoints);
 	getSurfacePoint = function(u, v) {
 		return nurbsSurface.getPoint(u, v);
 	};		
@@ -68,6 +56,23 @@ function MyPlane(scene, dimX, dimY, partsX, partsY){
 
 MyPlane.prototype = Object.create(CGFnurbsObject.prototype);
 MyPlane.prototype.constructor = MyPlane;
+
+/**
+ * Get knots vector according to degree
+ * @param {Integer} degree
+ */
+MyPlane.prototype.getKnotsVector = function(degree) { 
+	
+	var v = new Array();
+	for (var i=0; i<=degree; i++) {
+		v.push(0);
+	}
+	for (var i=0; i<=degree; i++) {
+		v.push(1);
+	}
+	return v;
+}
+
 
 /**
  * Texture amplify factors - no effect
