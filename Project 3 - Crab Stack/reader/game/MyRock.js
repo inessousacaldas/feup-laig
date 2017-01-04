84b68ff4c937c343788f252df65e094455c141c1
Miguel Lucas <cmiguel.lucas@gmail.com>
2017-01-04 18:00:08 +0000

    Halted rock

--- Project 3 - Crab Stack/reader/game/MyRock.js
+++ Project 3 - Crab Stack/reader/game/MyRock.js
@@ -12,10 +12,13 @@ function MyRock(scene){
 
     this.texture = new Texture(this.scene, 'scenes/textures/rock.jpg', 'rock');
 
-    this.topShell = new MyQuad(this.scene, 1, 1, 0.5, 100, 100);
-    /*this.bottomShell = new MyQuad(this.scene, 0.88, 0.7, 0.5, 100, 100);
+    this.rightSide = new MyQuad(this.scene, 1, 0.8, 1, 100, 100);
+    this.side1 = new MyQuad(this.scene, 0.3, 1, 1, 100, 100);
+    this.side2 = new MyQuad(this.scene, 0.2, 1, 1, 100, 100);
+    this.frontSide = new MyQuad(this.scene, 0.5, 1, 1, 100, 100);
+    this.side3 = new MyQuad(this.scene, 0.1, 1, 1, 100, 100);
 
-    this.bodyTriangle = new MyTriangle(this.scene, 0,0,0, 0.09, this.x, -0.14, 0.25, this.x, 0.5);
+    /*this.bodyTriangle = new MyTriangle(this.scene, 0,0,0, 0.09, this.x, -0.14, 0.25, this.x, 0.5);
     this.bodyTriangle2 = new MyTriangle(this.scene, 0,0,0, 0.16, 0, -0.64, 0.25, -this.x, -0.5);
     this.bottomEye = new MyFullCylinder(this.scene, 0.30, 0.05, 0.025);
     this.eye = new MySphere(this.scene, 0.1, 16, 16);
@@ -31,7 +34,9 @@ MyRock.prototype.constructor = MyRock;
  */
 MyRock.prototype.display = function() {
 
-    this.texture.bind();
+    //this.texture.bind();
+
+    this.scene.scale(4,4,4);
 
     this.scene.pushMatrix();
             this.scene.translate(0,0,5);
@@ -41,15 +46,36 @@ MyRock.prototype.display = function() {
 
     //Top shell right
    this.scene.pushMatrix();
-        //this.scene.translate(0,0.43,0);
-        //this.scene.rotate(Math.PI/2, 0,0,1);
-        //this.scene.rotate(30*deg2rad, 0,1,0);
-        this.topShell.display();
+        this.rightSide.display();
    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+        this.scene.translate(0.62,0,-0.071);
+        this.scene.rotate(30*deg2rad,0,1,0);
+        this.side1.display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+        this.scene.translate(0.775,0,-0.24);
+        this.scene.rotate(75*deg2rad,0,1,0);
+        this.side2.display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+        this.scene.translate(0.8,0,-0.57);
+        this.scene.rotate(90*deg2rad,0,1,0);
+        this.frontSide.display();
+    this.scene.popMatrix();
+
+    this.scene.pushMatrix();
+        this.scene.translate(0.783,0,-0.865);
+        this.scene.rotate(110*deg2rad,0,1,0);
+        this.side3.display();
+    this.scene.popMatrix();
 
    
 
-   this.texture.unbind();
+   //this.texture.unbind();
 
 }
 
